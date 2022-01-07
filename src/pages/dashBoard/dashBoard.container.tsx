985290fcc08624073abd7fc855b5e3b50ba55278
Hanjun Kwon <kwon96718@gmail.com>
2022-01-08 05:22:40 +0900

    feat: DashBoard Page 생성

---
+++ src/pages/dashBoard/dashBoard.container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DashBoardContainer(): JSX.Element {
+  return (
+    <div>
+      <span>hello</span>
+    </div>
+  );
+}
+
+export default DashBoardContainer;
---
+++ src/pages/dashBoard/dashBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DashBoard(): JSX.Element {
+  return (
+    <div>
+      <span>hello</span>
+    </div>
+  );
+}
+
+export default DashBoard;
